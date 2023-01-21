59c66f2003d28e6f5cbf238c13da495c4ad65c9d
paulocode <paulo@paulo.com>
2023-01-21 22:09:26 +0800

    fix sector_t not set

--- i2s.c
+++ i2s.c
@@ -175,6 +175,7 @@ void i2s_data_thread() {
 
 
     while (true) {
+        sector_t = sector;
         if (mutex_try_enter(&mechacon_mutex,0)) {
             while(!pio_sm_is_rx_fifo_empty(pio1, MECHACON_SM)) {
                 uint c = reverseBits(pio_sm_get_blocking(pio1, MECHACON_SM),8);
